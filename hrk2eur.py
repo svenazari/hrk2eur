56a2f8061e450b52e9df6dedf886908f3b16e2a2
Sven Azari <94923703+svenazari@users.noreply.github.com>
2022-09-22 19:38:11 +0200

    Add files via upload

---
+++ hrk2eur.py
@@ -0,0 +1,16 @@
+#Konvertiranje Kune u Euro prema fiksnom tečaju.
+#Za korištenje je potrebno imati instaliran Python 3.
+#Made by: SvenAzari
+#https://github.com/svenazari
+
+cur = float(input("IZNOS = ")) #iznos za konverziju
+
+rate = 7.53450 #fiksni tečaj za konverziju
+
+eur = round(cur / rate, 2) #konverzija u euro
+hrk = round(cur * rate, 2) #konverzija u kuna
+
+#ispis
+print("* * *")
+print(str(cur) + " HRK = " + str(eur) + " EUR")
+print(str(cur) + " EUR = " + str(hrk) + " HRK")
