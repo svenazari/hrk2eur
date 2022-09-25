3c0a9a082ef588d175a2487c53304449dd917c80
Sven Azari <94923703+svenazari@users.noreply.github.com>
2022-09-25 11:56:26 +0200

    Add files via upload

--- hrk2eur.py
+++ hrk2eur.py
@@ -3,14 +3,40 @@
 #Made by: SvenAzari
 #https://github.com/svenazari
 
-cur = float(input("IZNOS = ").replace(',','.')) #iznos za konverziju
+from os import system, name
 
-rate = 7.53450 #fiksni tečaj za konverziju
+def clear (): #čišćenje ekrana
+  if name == 'nt':
+      _ = system('cls')
+  else:
+      _ = system('clear')
 
-eur = round(cur / rate, 2) #konverzija u euro
-hrk = round(cur * rate, 2) #konverzija u kuna
+def conversion ():
+    clear () #očisti ekran
+    cur = float(input("IZNOS = ").replace(',','.')) #iznos za konverziju
 
-#ispis
-print("* * *")
-print(str(cur) + " HRK = " + str(eur) + " EUR")
-print(str(cur) + " EUR = " + str(hrk) + " HRK")
+    rate = 7.53450 #fiksni tečaj za konverziju 
+
+    eur = round(cur / rate, 2) #konverzija u euro
+    hrk = round(cur * rate, 2) #konverzija u kuna
+
+    #ispis
+    print("* * *")
+    print(str(cur) + " HRK = " + str(eur) + " EUR")
+    print(str(cur) + " EUR = " + str(hrk) + " HRK")
+    print("* * *")
+
+    fuex () #pozovi funkciju za odabir novog unosa ili izlaza
+
+def fuex (): #izlaz
+  ex = float(input ("[1 (novi unos), 2 (izlaz)]: "))
+  if ex == 1:
+    conversion ()
+  elif ex == 2:
+    clear ()
+    exit ()
+  else:
+    print ("Pogrešan unos!")
+    fuex ()
+
+conversion ()
